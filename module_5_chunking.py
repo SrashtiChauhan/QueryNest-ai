217d8d03bd0fe12e2a8b5d138f38b490f916dd42
Srashti Chauhan <srashtichauhan24@gmail.com>
2026-09-19 12:28:31 +0530

    character-based chunking

--- module_5_chunking.py
+++ module_5_chunking.py
@@ -5,12 +5,22 @@ Python is also popular for automation and artificial intelligence.
 Many developers use Python because its syntax is simple and readable.
 """
 
-chunk_size=2
-words=document.split()
+# chunk_size=2
+# words=document.split()
+# chunks=[]
+# for i in range(0, len(words), chunk_size):
+#     chunk = " ".join(words[i:i + chunk_size])
+#     chunks.append(chunk)
+
+chunk_size = 100
+chunk_overlap=20
 chunks=[]
-for i in range(0, len(words), chunk_size):
-    chunk = " ".join(words[i:i + chunk_size])
+start=0
+while start < len(document):
+    end=start+chunk_size
+    chunk=document[start:end]
     chunks.append(chunk)
+    start = end - chunk_overlap
 
 print("Original document:")
 print(document)
